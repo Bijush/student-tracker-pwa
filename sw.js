7338aef5e16ebced564d87125086dbd8cd75bb08
Bijush <38435589+Bijush@users.noreply.github.com>
2025-05-29 12:34:16 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,38 @@
-const CACHE_NAME = 'student-tracker-cache-v1';
+const CACHE_NAME = "attendance-cache-v1";
 const urlsToCache = [
-  'index.html',
-  'style.css',
-  'app.js',
-  'manifest.json',
-  'icon.png'
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      )
+    )
+  );
+});
+then(response => response || fetch(event.request))
   );
 });
